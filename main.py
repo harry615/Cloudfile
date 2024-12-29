37247c9695103197caf3c45c1d63a25fd5906221
Harry Cole <harry61551@gmail.com>
2024-12-29 16:49:44 +0000

    Modularising database

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from typing import Annotated
-
 from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, select
 
+from database import engine, create_db_and_tables
 
 class HeroBase(SQLModel):
     name: str = Field(index=True)
@@ -26,17 +26,6 @@ class HeroUpdate(HeroBase):
     name: str | None = None
     age: int | None = None
     secret_name: str | None = None
-
-
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
-
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
 
 
 def get_session():
