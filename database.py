9d6a653acabc46daf74de8f797f590a8abc49779
Harry Cole <harry61551@gmail.com>
2025-01-01 12:21:19 +0000

    Succesfully connected to azure database (MySQL) :)

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from sqlmodel import SQLModel, create_engine
-from models import SQLModel, Hero
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+# Azure Database connection string (example for Azure SQL Database)
+DATABASE_URL = "mysql+pymysql://adminsql:6EkwFTZqGBv6a5i@harry61551sql.mysql.database.azure.com/sqldatabase"
 
+
+# Create the engine
+engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, select
-
 from database import engine, create_db_and_tables
 from routes import router
 
-@asynccontextmanager
+
 async def lifespan(app: FastAPI):
-    # Startup logic
-    await create_db_and_tables()
+    create_db_and_tables()
     yield
-    # Shutdown logic would go here, if needed
 
 app = FastAPI(lifespan=lifespan)
 
