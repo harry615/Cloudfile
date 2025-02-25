4cea6828e73d705ce62e954ec48e64e28346080e
Harry Cole <harry61551@gmail.com>
2025-02-25 16:50:49 +0000

    Update

--- database.py
+++ database.py
@@ -2,8 +2,7 @@ import os
 from sqlmodel import SQLModel, create_engine
 from models import User, FileRecord  # Added imports for schema creation
 
-# Azure Database connection string (example for Azure SQL Database)
-#DATABASE_URL = "mysql+pymysql://adminsql:6EkwFTZqGBv6a5i@harry61551sql.mysql.database.azure.com/sqldatabase?ssl_ca=/app/BaltimoreCyberTrustRoot.crt.pem"
+# Azure Database connection string 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 # Create the engine
